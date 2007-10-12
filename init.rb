8771a632dc26a7782800347993869c964133ea29
Jeremy Kemper <jeremy@bitsweat.net>
2007-10-12 09:18:00 +0000

    Ruby 1.9 forward compat

--- init.rb
+++ init.rb
@@ -1,3 +1,3 @@
 $:.unshift "#{File.dirname(__FILE__)}/lib"
 require 'active_record/acts/list'
-ActiveRecord::Base.send :include, ActiveRecord::Acts::List
+ActiveRecord::Base.class_eval { include ActiveRecord::Acts::List }
