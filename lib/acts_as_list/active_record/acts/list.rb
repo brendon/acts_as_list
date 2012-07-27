7efbbaf4c04ef688a0f027ee1ba6f77111ef6342
Kevin Moore <kevin@thinkpixellab.com>
2012-07-27 11:19:37 -0700

    Replaced usage of update_attribute with update_attribute!
    Fixed deprecation warning in Rails 3.2.7
    
    Closes #44

--- lib/acts_as_list/active_record/acts/list.rb
+++ lib/acts_as_list/active_record/acts/list.rb
@@ -136,20 +136,20 @@ module ActiveRecord
         def remove_from_list
           if in_list?
             decrement_positions_on_lower_items
-            update_attribute position_column, nil
+            update_attributes! position_column => nil
           end
         end
 
         # Increase the position of this item without adjusting the rest of the list.
         def increment_position
           return unless in_list?
-          update_attribute position_column, self.send(position_column).to_i + 1
+          update_attributes! position_column => self.send(position_column).to_i + 1
         end
 
         # Decrease the position of this item without adjusting the rest of the list.
         def decrement_position
           return unless in_list?
-          update_attribute position_column, self.send(position_column).to_i - 1
+          update_attributes! position_column => self.send(position_column).to_i - 1
         end
 
         # Return +true+ if this object is the first in the list.
@@ -230,12 +230,12 @@ module ActiveRecord
 
           # Forces item to assume the bottom position in the list.
           def assume_bottom_position
-            update_attribute(position_column, bottom_position_in_list(self).to_i + 1)
+            update_attributes!(position_column => bottom_position_in_list(self).to_i + 1)
           end
 
           # Forces item to assume the top position in the list.
           def assume_top_position
-            update_attribute(position_column, acts_as_list_top)
+            update_attributes!(position_column => acts_as_list_top)
           end
 
           # This has the effect of moving all the higher items up one.
@@ -307,14 +307,14 @@ module ActiveRecord
             else
               increment_positions_on_lower_items(position)
             end
-            self.update_attribute(position_column, position)
+            self.update_attributes!(position_column => position)
           end
 
           # used by insert_at_position instead of remove_from_list, as postgresql raises error if position_column has non-null constraint
           def store_at_0
             if in_list?
               old_position = send(position_column).to_i
-              update_attribute(position_column, 0)
+              update_attributes!(position_column => 0)
               decrement_positions_on_lower_items(old_position)
             end
           end
