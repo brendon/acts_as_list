4281f18a3ae3a8239e050cae46dd4b1bb61d36c2
Akira Matsuda <ronnie@dio.jp>
2014-03-28 18:43:50 +0900

    Let AR::Base lazily loaded if Railtie is available

--- lib/acts_as_list.rb
+++ lib/acts_as_list.rb
@@ -1,24 +1,17 @@
 require 'acts_as_list/active_record/acts/list'
 
 module ActsAsList
-  if defined? Rails::Railtie
+  begin
     require 'rails'
+
     class Railtie < Rails::Railtie
       initializer 'acts_as_list.insert_into_active_record' do
         ActiveSupport.on_load :active_record do
-          ActsAsList::Railtie.insert
+          ActiveRecord::Base.send(:include, ActiveRecord::Acts::List)
         end
       end
     end
-  end
-
-  class Railtie
-    def self.insert
-      if defined?(ActiveRecord)
-        ActiveRecord::Base.send(:include, ActiveRecord::Acts::List)
-      end
-    end
+  rescue LoadError
+    ActiveRecord::Base.send(:include, ActiveRecord::Acts::List) if defined?(ActiveRecord)
   end
 end
-
-ActsAsList::Railtie.insert
