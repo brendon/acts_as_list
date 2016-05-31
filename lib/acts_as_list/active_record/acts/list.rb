d3ea2a3d5a7322282348b7148d62513e5589596e
Wen Hsiao <wenh@mm.st>
2016-05-31 18:54:55 +0900

    Removed duplicated assignment

--- lib/acts_as_list/active_record/acts/list.rb
+++ lib/acts_as_list/active_record/acts/list.rb
@@ -346,7 +346,6 @@ module ActiveRecord
 
           # Returns the bottom item
           def bottom_item(except = nil)
-            conditions = scope_condition
             conditions = except ? "#{self.class.primary_key} != #{self.class.connection.quote(except.id)}" : {}
             acts_as_list_list.in_list.where(
               conditions
