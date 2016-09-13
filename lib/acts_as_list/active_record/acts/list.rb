dc42a8bb9b811f8353d3557c887e98d4536bd30e
Dmitry Maksyoma <ledestin@gmail.com>
2016-09-13 03:52:35 -0400

    Refactoring: extract CallbackDefiner module

--- lib/acts_as_list/active_record/acts/list.rb
+++ lib/acts_as_list/active_record/acts/list.rb
@@ -3,6 +3,7 @@ require_relative "scope_method_definer"
 require_relative "top_of_list_method_definer"
 require_relative "add_new_at_method_definer"
 require_relative "update_position_method_definer"
+require_relative "callback_definer"
 
 module ActiveRecord
   module Acts #:nodoc:
@@ -54,19 +55,7 @@ module ActiveRecord
             caller_class
           end
 
-          before_validation :check_top_position
-
-          before_destroy :lock!
-          after_destroy :decrement_positions_on_lower_items
-
-          before_update :check_scope
-          after_update :update_positions
-
-          after_commit :clear_scope_changed
-
-          if add_new_at.present?
-            before_create "add_to_list_#{add_new_at}".to_sym
-          end
+          CallbackDefiner.call(caller_class, add_new_at)
 
           include ::ActiveRecord::Acts::List::InstanceMethods
         end
