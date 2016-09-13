5b17c51c11489330440b31d89f1448e481806ec8
Dmitry Maksyoma <ledestin@gmail.com>
2016-09-13 05:55:58 -0400

    Add a missing dot in comments

--- lib/acts_as_list/active_record/acts/list.rb
+++ lib/acts_as_list/active_record/acts/list.rb
@@ -41,7 +41,7 @@ module ActiveRecord
         # * +top_of_list+ - defines the integer used for the top of the list. Defaults to 1. Use 0 to make the collection
         #   act more like an array in its indexing.
         # * +add_new_at+ - specifies whether objects get added to the :top or :bottom of the list. (default: +bottom+)
-        #                   `nil` will result in new items not being added to the list on create
+        #                   `nil` will result in new items not being added to the list on create.
         def acts_as_list(options = {})
           configuration = { column: "position", scope: "1 = 1", top_of_list: 1, add_new_at: :bottom}
           configuration.update(options) if options.is_a?(Hash)
