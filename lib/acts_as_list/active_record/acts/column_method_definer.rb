eb08a16860aaad29e16d4020d5dee58041cae563
Dmitry Maksyoma <ledestin@gmail.com>
2016-09-23 23:12:32 +1200

    Refactoring: use a better variable name
    Rename column -> position_column to make it clear what kind of column we're
    talking about.

--- lib/acts_as_list/active_record/acts/column_method_definer.rb
+++ lib/acts_as_list/active_record/acts/column_method_definer.rb
@@ -1,25 +1,25 @@
 module ActiveRecord::Acts::List::ColumnMethodDefiner #:nodoc:
-  def self.call(caller_class, column)
+  def self.call(caller_class, position_column)
     caller_class.class_eval do
       attr_reader :position_changed
 
       define_method :position_column do
-        column
+        position_column
       end
 
-      define_method :"#{column}=" do |position|
-        write_attribute(column, position)
+      define_method :"#{position_column}=" do |position|
+        write_attribute(position_column, position)
         @position_changed = true
       end
 
       # only add to attr_accessible
       # if the class has some mass_assignment_protection
       if defined?(accessible_attributes) and !accessible_attributes.blank?
-        attr_accessible :"#{column}"
+        attr_accessible :"#{position_column}"
       end
 
       define_singleton_method :quoted_position_column do
-        @_quoted_position_column ||= connection.quote_column_name(column)
+        @_quoted_position_column ||= connection.quote_column_name(position_column)
       end
 
       define_singleton_method :quoted_position_column_with_table_name do
