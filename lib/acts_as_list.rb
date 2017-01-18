b5a54aad0ee70ce8e601213e2d2f4554a7a21187
Brendon Muir <brendon@spike.net.nz>
2017-01-18 20:13:59 +1300

    Change require order

--- lib/acts_as_list.rb
+++ lib/acts_as_list.rb
@@ -1,7 +1,7 @@
+require "acts_as_list/active_record/acts/list"
 require "acts_as_list/active_record/acts/column_method_definer"
 require "acts_as_list/active_record/acts/scope_method_definer"
 require "acts_as_list/active_record/acts/top_of_list_method_definer"
 require "acts_as_list/active_record/acts/add_new_at_method_definer"
 require "acts_as_list/active_record/acts/aux_method_definer"
 require "acts_as_list/active_record/acts/callback_definer"
-require 'acts_as_list/active_record/acts/list'
