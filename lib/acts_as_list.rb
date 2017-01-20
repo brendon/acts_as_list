88d9af1e2ba0f5e13b57f821ed52acf5d0d558ed
Andrey Zharikov <andrey@zharikov.pro>
2017-01-21 00:20:27 +0300

    removed redundant require

--- lib/acts_as_list.rb
+++ lib/acts_as_list.rb
@@ -5,6 +5,5 @@ require "acts_as_list/active_record/acts/top_of_list_method_definer"
 require "acts_as_list/active_record/acts/add_new_at_method_definer"
 require "acts_as_list/active_record/acts/aux_method_definer"
 require "acts_as_list/active_record/acts/callback_definer"
-require "acts_as_list/active_record/acts/sequential_updates_method_definer"
-require 'acts_as_list/active_record/acts/list'
 require 'acts_as_list/active_record/acts/no_update'
+require "acts_as_list/active_record/acts/sequential_updates_method_definer"
