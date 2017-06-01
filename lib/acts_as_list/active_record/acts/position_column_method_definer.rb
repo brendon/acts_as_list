99087a45141d81f267a165e7db824822eb9285de
Dmitry Maksyoma <ledestin@gmail.com>
2017-06-01 18:54:33 +1200

    Refactor: extract variable "quoted_now"

--- lib/acts_as_list/active_record/acts/position_column_method_definer.rb
+++ lib/acts_as_list/active_record/acts/position_column_method_definer.rb
@@ -55,9 +55,11 @@ module ActiveRecord::Acts::List::PositionColumnMethodDefiner #:nodoc:
 
       define_method :touch_record_sql do
         connection = self.class.connection
+        quoted_now = connection.quote(connection.quoted_date(
+          current_time_from_proper_timezone))
 
         timestamp_attributes_for_update_in_model.inject("") do |sql, attr|
-          sql << ", #{connection.quote_column_name(attr)} = #{connection.quote(connection.quoted_date(current_time_from_proper_timezone))}"
+          sql << ", #{connection.quote_column_name(attr)} = #{quoted_now}"
         end
       end
     end
