a35daedc79a26cfbe7045d97ed977368ee2c002b
Brendon Muir <brendon@spike.net.nz>
2018-08-14 09:56:47 +1200

    Fix the use of a class with acts_as_list twice causing method overwrite warnings

--- test/test_no_update_for_subclasses.rb
+++ test/test_no_update_for_subclasses.rb
@@ -2,21 +2,21 @@
 
 require 'helper'
 
-class TodoItem < ActiveRecord::Base
+class MasterItem < ActiveRecord::Base
   acts_as_list
 end
 
-class SubTodoItem < TodoItem; end
+class SlaveItem < MasterItem; end
 
 class NoUpdateForSubclassesTestCase < Minitest::Test
   def setup
-    ActiveRecord::Base.connection.create_table :todo_items do |t|
+    ActiveRecord::Base.connection.create_table :master_items do |t|
       t.column :position, :integer
       t.column :type, :string
     end
 
     ActiveRecord::Base.connection.schema_cache.clear!
-    [TodoItem, SubTodoItem].each(&:reset_column_information)
+    [MasterItem, SlaveItem].each(&:reset_column_information)
     super
   end
 
@@ -30,7 +30,7 @@ class NoUpdateForSubclassesTest < NoUpdateForSubclassesTestCase
   def setup
     super
     @item_1, @item_2 = (1..2).map do |counter|
-      SubTodoItem.create!(position: counter)
+      SlaveItem.create!(position: counter)
     end
   end
 
@@ -41,14 +41,14 @@ class NoUpdateForSubclassesTest < NoUpdateForSubclassesTestCase
   end
 
   def test_no_update_for_subclass_instances_with_no_update_on_superclass
-    TodoItem.acts_as_list_no_update { @item_1.update_attributes(position: 2) }
+    MasterItem.acts_as_list_no_update { @item_1.update_attributes(position: 2) }
 
     assert_equal 2, @item_1.reload.position
     assert_equal 2, @item_2.reload.position
   end
 
   def test_no_update_for_subclass_instances_with_no_update_on_subclass
-    SubTodoItem.acts_as_list_no_update { @item_1.update_attributes(position: 2) }
+    SlaveItem.acts_as_list_no_update { @item_1.update_attributes(position: 2) }
 
     assert_equal 2, @item_1.reload.position
     assert_equal 2, @item_2.reload.position
