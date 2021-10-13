2a58ad53ed68f51fc039be649f10c67baefe5cee
Guruprasad <guruprasad.murali@freshworks.com>
2021-10-13 20:51:25 +0530

    Enhancement: Execute show table queries through schema cache class
    Executing show table queries through schema cache class allows active record connections to cache show table queries and prevents from firing multiple times.

--- lib/acts_as_list/active_record/acts/sequential_updates_method_definer.rb
+++ lib/acts_as_list/active_record/acts/sequential_updates_method_definer.rb
@@ -6,11 +6,12 @@ module ActiveRecord::Acts::List::SequentialUpdatesMethodDefiner #:nodoc:
       define_method :sequential_updates? do
         if !defined?(@sequential_updates)
           if sequential_updates_option.nil?
+            schema_connection = caller_class.connection.respond_to?(:schema_cache) ? caller_class.connection.schema_cache : caller_class.connection
             table_exists =
               if active_record_version_is?('>= 5')
-                caller_class.connection.data_source_exists?(caller_class.table_name)
+                schema_connection.data_source_exists?(caller_class.table_name)
               else
-                caller_class.connection.table_exists?(caller_class.table_name)
+                schema_connection.table_exists?(caller_class.table_name)
               end
             index_exists = caller_class.connection.index_exists?(caller_class.table_name, column, unique: true)
             @sequential_updates = table_exists && index_exists
