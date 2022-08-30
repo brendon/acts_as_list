22537203e31de7091bb06c9e9fb0c27fed59fbb8
Durgesh Singh <durgesh@bluesapling.com>
2022-08-30 21:08:59 +0530

    Add spec for default scope with uniqueness constraint on the position column

--- test/test_default_scope_with_select.rb
+++ test/test_default_scope_with_select.rb
@@ -10,6 +10,8 @@ class DefaultScopeWithSelectTest < Minitest::Test
     ActiveRecord::Base.connection.create_table :animals do |t|
       t.column :position, :integer
       t.column :name, :string
+
+      t.index :position, unique: true
     end
 
     ActiveRecord::Base.connection.schema_cache.clear!
@@ -29,5 +31,19 @@ class DefaultScopeWithSelectTest < Minitest::Test
     assert_equal 1, animal1.position
     assert_equal 2, animal2.position
     assert_equal 3, animal3.position
+  end
+
+  def test_default_scope_on_update
+    animal1 = Animal.create name: 'Fox', position: 1
+    animal2 = Animal.create name: 'Panda', position: 2
+    animal3 = Animal.create name: 'Wildebeast', position: 3
+
+    animal1.update position: 2
+    animal2.update position: 3
+    animal3.update position: 1
+
+    assert_equal 2, animal1.reload.position
+    assert_equal 3, animal2.reload.position
+    assert_equal 1, animal3.reload.position
   end
 end
